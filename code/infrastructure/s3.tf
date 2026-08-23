aa7cc28dead1871ee4b454fcb2c86cfdfc9d28d5
misondroid <misondroidjpn@gmail.com>
2026-08-23 11:22:18 +0900

    パッケージ寛永をuvに移行

--- code/infrastructure/s3.tf
+++ code/infrastructure/s3.tf
@@ -2,3 +2,13 @@ data "aws_s3_bucket" "management_bucket" {
   bucket = var.management_bucket
 }
 
+resource "aws_s3_bucket_notification" "management_bucket_notification" {
+  bucket = data.aws_s3_bucket.management_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.app.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "app/${var.app_env}/"
+    filter_suffix       = ".zip"
+  }
+}
--- code/infrastructure/variables.tf
+++ code/infrastructure/variables.tf
@@ -1,3 +1,13 @@
+variable "app_lambda_function_name" {
+  type = string
+  description = "The name of the app lambda function"
+  default = "picoddns"
+}
+variable "app_env" {
+  type = string
+  description = "The environment of the app"
+  default = "dev"
+}
 variable "management_bucket" {
   type = string
   description = "The name of the management bucket"
